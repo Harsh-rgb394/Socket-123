3d99b3aa187e014e3122356330042f33347f267a
Harsha <harshitharsha778@gmail.com>
2023-11-25 01:11:44 +0530

    updated

--- src/pages/chat/Talk.js
+++ src/pages/chat/Talk.js
@@ -1,6 +1,53 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import query from "query-string"
+import { useLocation } from 'react-router-dom'
+import io from "socket.io-client"
 
+let socket;
 const Talk = () => {
+  const [name,setname]=useState(' ');
+  const [room,setroom]=useState(' ');
+  const [message,setmessage]=useState(' ');
+  const [messages,setmessages]=useState([]);
+  const location=useLocation();
+  const Endpoint='localhost:5000';
+
+  useEffect(() => {
+    const {name,room}=query.parse(location.search);
+
+    socket =io(Endpoint);
+
+    setname(name);
+    setroom(room);
+    // console.log(info);
+    // yeh jo hai sara paramter or valrbules part of ulr 
+    // console.log(location.search);
+    // yeh to hai unko ko object ke form mein bana dega a:b c:d
+
+    socket.emit("meet",{name,room});
+
+
+    // return()=>{
+    //   socket.emit("disconnect");
+
+    //   socket.off();
+
+    // }
+  
+    
+  },[Endpoint,location.search])
+
+  useEffect(() => {
+    socket.on("message",(message)=>{
+      setmessages({...messages,message});
+    })
+    
+  
+
+  }, [messages])
+  
+
+  
   return (
     <div>first chat
     </div>
--- src/pages/join/Meet.js
+++ src/pages/join/Meet.js
@@ -3,29 +3,33 @@ import { Link } from 'react-router-dom'
 
 
 const Meet = () => {
-  const [name,setname]=useState(" ");
-  const [room,setroom]=useState(" ");
+  const [name,setname]=useState(' ');
+  const [room,setroom]=useState(' ');
   
 //   console.log(name);
 //   console.log(room);
   return (
-    <div className='flex justify-center text-center h-screen items-center '>
+    <div className='flex justify-center text-center h-screen items-center  bg-slate-300'>
         <div className='w-1/4'>
             <h2 className='text-orange-600	text-3xl	border-b-2 border-solid   border-black py-2	'> Meet</h2>
         <div>
-            <input placeholder='Enter your name' className=' border-2 border-solid border-black mt-3 	  px-3 py-1 w-3/4 ' type='text' onClick={(e)=>setname(e.target.value)}/>
+            <input placeholder='Enter your name' className=' border-2 border-solid border-black mt-3 	  px-3 py-1 w-full ' type='text' onChange={(e)=>setname(e.target.value)}/>
 
         </div>
         <div>
-            <input placeholder='Enter your room' className='border-2 border-solid border-black mt-3 	  px-3 py-1 w-3/4' type='text' onClick={(e)=>setroom(e.target.value)}/>
+            <input placeholder='Enter your room' className='border-2 border-solid border-black mt-3 	  px-3 py-1 w-full' type='text' onChange={(e)=>setroom(e.target.value)}/>
 
         </div>
+        {/* onClick={(e)=>(!name || !room) ?e.preventDefault():null} */}
+        
 
-        <Link to={`/talk?name=${name}&room=${room}`}>
-            <button className='text-blue-500 uppercase bg-red-100	 border-2 border-solid  border-white mt-3 w-3/4 rounded-md p-1' type='submit'>
+        <Link onClick={(e)=>(!name || !room) ?e.preventDefault():null} to={`/chat?name=${name}&room=${room}`}>
+            
+        <button className='text-blue-500 uppercase bg-red-100	 border-2 border-solid  border-white mt-3 w-full rounded-md p-1' type='submit'>
                 Meet up
             </button>
         </Link>
+
         </div>
         </div>
   )
